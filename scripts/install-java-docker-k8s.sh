2c3cb9315756b32bc93e0184c8d3e82329a4b978
Nagham Mohamed <naghamfathy94@gmail.com>
2025-05-09 23:19:05 +0300

    add script

---
+++ scripts/install-java-docker-k8s.sh
@@ -0,0 +1,39 @@
+#install java
+sudo apt update
+sudo apt install openjdk-17-jre -y
+java -version
+
+#install docker
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+sudo systemctl enable docker
+sudo systemctl start docker
+
+# Allow your WSL user to run Docker without 'sudo'
+sudo usermod -aG docker $USER
+
+
+# install k8s
+# Install Required Dependencies
+sudo apt-get update
+sudo apt-get install -y curl wget apt-transport-https
+
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# Verify installation
+kubectl version --client
